f6a529f4d7db30da5ef327145173c347a155b103
olarich11 <olarich11@gmail.com>
2023-03-31 05:47:25 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,9 +10,9 @@ char *leet(char *n)
 	char s1[] = "aAeEo0tTlL";
 	char s2[] = "4433007711";
 
-	for (i = 0; n[i] != '\0'; i++)
+	for (i = 0 ; n[i] != '\0' ; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0 ; j < 10 ; j++)
 		{
 			if (n[i] == s1[j])
 			{

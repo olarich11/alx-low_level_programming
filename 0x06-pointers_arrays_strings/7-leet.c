bf25f741df111f012220061b3dc1bc8b6916bb68
olarich11 <olarich11@gmail.com>
2023-03-30 20:41:34 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,23 +4,20 @@
  * @n: input value
  * Return: n value
  */
-
 char *leet(char *n)
 {
 	int i, j;
-
 	char s1[] = "aAeEo0tTlL";
 	char s2[] = "4433007711";
 
-	for (i = 0 ; n[i] != '\0' ; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (j = 0 ; j < 10 ;j++)
+		for (j = 0; j < 10; j++)
 		{
 			if (n[i] == s1[j])
-			{	
+			{
 				n[i] = s2[j];
 			}
-
 		}
 	}
 	return (n);

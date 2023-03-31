b1ebb0b78cbb950e66ad36b5f15dae8f07434d8a
olarich11 <olarich11@gmail.com>
2023-03-31 01:36:41 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_number - print numbers chars
- * @n: integer params
- * Return: 0
+ * @n: integer to be printed
+ *
  */
 
 void print_number(int n)

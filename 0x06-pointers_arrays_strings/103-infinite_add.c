1cd4fe559342ed99b8dc25696cdd18daf914d56d
olarich11 <olarich11@gmail.com>
2023-03-31 21:15:11 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,48 +1,53 @@
 #include "main.h"
 /**
- * main- function that adds two numbers
- * Return: 0 (success)
+ * infinite_add- add two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: result
+ * @size_r: result length
+ * Return: sum
+ *
  */
 
-int main(void)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int val1 = 0, val2 = 0, temp_tot = 0;
+	/* local variable declaration */
+	int i  = 0, j = 0, k, l = 0, f, s, d = 0;
 
-	while (*(n1 + i) != '\0')
+	while (n1[i] != '\0')
 		i++;
-
-	while (*(n2 + j) != '\0')
+	while (n2[j] != '\0')
 		j++;
-	i--;
-	j--;
-	if (j >= size_r || i >= size_r)
+	if (i > j)
+		l = i;
+	else
+		l = j;
+	if (l + 1 > size_r)
 		return (0);
-
-	while (j >= || i >= 0 || overflow == 1)
+	r[1] = '\0';
+	for (k = l - 1 ; k >= 0 ; k--)
 	{
-		if (i < 0)
-			val1 = 0;
-		else
-			val1 = *(n1 + i) - '0';
-		if (j < 0)
-			val2 = 0;
-		else
-			val2 = *(n2 + j) - '0';
-		temp_tot = val1 + val2 + overflow;
-		if (temp_tot >= 10)
-			overflow = 1;
-		else
-			overflow = 0;
-		if (digits >= (size_r - 1))
-			return (0);
-		*(r + digits) = (temp_tot % 10) + '0';
-		digits++;
-		j--;
 		i--;
+		j--;
+		if (i >= 0)
+			f = n1[i] - '0';
+		else
+			f = 0;
+		if (j >= 0)
+			s = n2[j] - '0';
+		else
+			s = 0;
+		r[k] = (f + s + d) % 10 + '0';
+		d = (f + s + d) / 10;
 	}
-	if (digits == size_r)
-		return (0);
-	*(r + digits) = '\0';
-	rev_string(r);
+	if (d == 1)
+	{
+		r[l + 1] = '\0';
+		if (l + 2 > size_r)
+			return (0);
+		while (l-- >= 0)
+			r[l + 1] = r[l];
+		r[0] = d + '0';
+	}
 	return (r);
 }

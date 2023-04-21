fe2468e8ddb97a87f9431226ad4b62c3be11bf0f
olarich11 <olarich11@gmail.com>
2023-04-21 07:14:18 -0700

    created 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,10 +9,9 @@
  *
  * Return: Always 0 (Sussess)
  */
-int main(int __attribute__((__unused__))argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int num1, num2;
-	char *op;
+	int (*oprt)(int, int);
 
 	if (argc != 4)
 	{
@@ -20,24 +19,14 @@ int main(int __attribute__((__unused__))argc, char *argv[])
 		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
-	op = argv[2];
-	num2 = atoi(argv[3]);
+	oprt = get_op_func(argv[2]);
 
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	if (!oprt)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*op == '/' && num2 == 0) ||
-			(*op == '%' && num2 == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	printf("%d\n", get_op_func(op)(num1, num2));
-
 	return (0);
 }

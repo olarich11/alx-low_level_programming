808468454b42377005cfbfb4a4aadb5cb865f172
olarich11 <olarich11@gmail.com>
2023-05-01 08:29:48 -0700

    4-free_listint.c c 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,14 +6,14 @@
  *
  * @head: Pointer to head list to be freed
  *
- * Return: void should be in Success
  */
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	while (head == NULL)
+	temp = head;
 
+	while (head == NULL)
 	{
 		temp = head;
 		head = head->next;

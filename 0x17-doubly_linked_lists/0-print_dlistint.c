86745a0cbc0f1a1e791797aeeeb0c292693257b5
olarich11 <olarich11@gamil.com>
2023-08-17 21:19:29 -0700

    Task 0 updatedbugs

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,22 +1,33 @@
 #include "lists.h"
 
-size_t print_dlistint(const dlistint_t *h) {
-	
+/**
+ * print_dlistint - Prints all elements of a dlistint_t list.
+ *
+ * @h: Pointer to the head of the list.
+ * Return: Number of nodes.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+
 	size_t count = 0;
-	
-	if (h == NULL) {
-		return 0;
+
+	if (h == NULL)
+	{
+		return (0);
 	}
-	
-	while (h->prev != NULL) {
+
+	while (h->prev != NULL)
+	{
 		h = h->prev;
 	}
 
-	while (h != NULL) {
+	while (h != NULL)
+	{
 		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
-	return count;
+	return (count);
 }
 
---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,67 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Inserts a new node at a given position
+ *
+ * @h: The pointer head of the list
+ * @idx: Index of the new node
+ * @n: vThe alue of the new node
+ * Return: The address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node;
+	
+	dlistint_t *iterator;
+
+	unsigned int i;
+
+	new_node = NULL;
+
+	if (idx == 0)
+	{
+		new_node = add_dnodeint(h, n);
+	}
+	
+	else
+	{
+		iterator = *h;
+		i = 1;
+
+		if (iterator != NULL)
+			while (iterator->prev != NULL)
+				iterator = iterator->prev;
+	}
+
+	while (iterator != NULL)
+	{
+		if (i == idx)
+		{
+			if (iterator->next == NULL)
+			{
+				new_node = add_dnodeint_end(h, n);
+			}
+
+			els
+			{
+				new_node = malloc(sizeof(dlistint_t));
+	
+				if (new_node != NULL)
+				{
+					new_node->n = n;
+					new_node->next = iterator->next;
+                        new_node->prev = iterator;
+                        iterator->next->prev = new_node;
+                        iterator->next = new_node;
+                    }
+                }
+                break;
+            }
+            iterator = iterator->next;
+            i++;
+        }
+    }
+
+    return new_node;
+}
+
